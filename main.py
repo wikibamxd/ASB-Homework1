4aacd0c06bac5186f42b1233fbf160af9ff54ebf
wikibamxd <wikibamxd@gmail.com>
2026-03-05 12:45:18 +0000

    Updated the python file

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("teste")
+"""
+Execução:
+python3 my_script.py «arg1» «arg2»
+Estrutura:
+
+
+Output (FASTA e STDOUT)
+"""
+import argparse
+
+#command line arguments
+parser = argparse.ArgumentParser(description="im not sure yet")
+parser.add_argument("-db", required=True, help="Database to use")
+parser.add_argument("-query", required=True, help="Search query")
+parser.add_argument("-out1", required=True, help="Output directory")
+args = parser.parse_args()
